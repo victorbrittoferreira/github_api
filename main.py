251dc6475b1b72dfc09b49261245d0a8d7430080
victorbrittoferreira@gmail.com <victorbrittoferreira@gmail.com>
2023-02-13 01:02:03 -0300

    func updated: typing. next step construct class and tests

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 import requests
 from requests.exceptions import HTTPError
+from typing import Generator, Iterable, Tuple
 
-
-def ask_user():
-    # return str(input("Input username:"))
-    return 'victorbrittoferreira'
-
-
-# user_name = ask_user()
 
 # TODO: colocar um async e exception customizada
 
+def url_format(user_name: str, url_user: str, url_user_repo: str) -> Tuple[str, str]:
+    named_url_user = url_user.replace('user_name', user_name)
+    named_url_user_repo = url_user_repo.replace('user_name', user_name)
 
-def request_github(user_name):
-    url = f"https://api.github.com/users/{user_name}"
-    url_repo = f'https://api.github.com/users/{user_name}/repos'
+    return named_url_user, named_url_user_repo
+
+
+def request_github(url_user: str, url_user_repo: str) -> Tuple[dict, list]:
     try:
-        requests_user = (url, url_repo)
+        requests_user = (url_user, url_user_repo)
         user_repo_data = []
         for url in requests_user:
             response = requests.get(url)
@@ -26,50 +24,42 @@ def request_github(user_name):
                 response.raise_for_status()
     except HTTPError:
         raise
-    return user_repo_data
+    return tuple(user_repo_data)
 
-
-# user_data = request_github(user_name)
 
 # TODO: colocar exception customizada
 
 
-def collect_user_summary(user_data):
+def collect_user_summary(user_data: dict) -> Tuple[str | int]:
     try:
         user_summary = (
-            user_data['name'], user_data['url'], user_data['public_repos'],
-            user_data['followers'], user_data['following']
+            user_data["name"],
+            user_data["url"],
+            user_data["public_repos"],
+            user_data["followers"],
+            user_data["following"],
         )
     except KeyError:
         raise
     return user_summary
 
 
-# user_summary = collect_user_summary(user_data[0])
-
-
 # TODO: colocar exception customizada
-def collect_user_repositories(repositories):
+def collect_user_repositories(repositories: list) -> Generator[int, str, Iterable]:
     try:
-        list_repo = [repo['name'] for repo in repositories]
+        list_repo = (repo["name"] for repo in repositories)
     except KeyError:
         raise
-    return tuple(list_repo)
-
-
-# repository_list = collect_user_repositories(user_data[1])
+    return list_repo
 
 
 def data_dump_to_txt(user_summary: tuple, repository_list: tuple) -> None:
     try:
-        with open('github_user_dossier.txt', 'w') as f:
+        with open("github_user_dossier.txt", "w") as f:
             f.write(
                 f"{user_summary[0]}, {user_summary[1]}, {user_summary[2]},"
-                f" {user_summary[3]}, {user_summary[4]}, {repository_list}"
+                f" {user_summary[3]}, {user_summary[4]}, {[*repository_list]}"
             )
     except IndexError:
         raise
     return None
-
-
-# data_dump_to_txt(user_summary, repository_list)
