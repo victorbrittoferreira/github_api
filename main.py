f98030afc2ed3721118b19230ef3955014d85775
victorbrittoferreira@gmail.com <victorbrittoferreira@gmail.com>
2023-02-12 23:04:31 -0300

    pre-commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from requests.exceptions import HTTPError
 
 
 def ask_user():
@@ -6,53 +7,69 @@ def ask_user():
     return 'victorbrittoferreira'
 
 
-user_name = ask_user()
+# user_name = ask_user()
+
+# TODO: colocar um async e exception customizada
 
 
 def request_github(user_name):
+    url = f"https://api.github.com/users/{user_name}"
+    url_repo = f'https://api.github.com/users/{user_name}/repos'
     try:
-        user_repo_data = []
-        url = f"https://api.github.com/users/{user_name}"
-        url_repo = f'https://api.github.com/users/{user_name}/repos'
         requests_user = (url, url_repo)
+        user_repo_data = []
         for url in requests_user:
-            response = requests.get(url).json()
-            user_repo_data.append(response)
-    except:
-        ...
+            response = requests.get(url)
+            if response.status_code == 200:
+                user_repo_data.append(response.json())
+            else:
+                response.raise_for_status()
+    except HTTPError:
+        raise
     return user_repo_data
 
 
-user_data = request_github(user_name)
+# user_data = request_github(user_name)
+
+# TODO: colocar exception customizada
 
 
 def collect_user_summary(user_data):
-    user_sumary = (
-        user_data['name'], user_data['url'], user_data['public_repos'],
-        user_data['followers'], user_data['following']
-    )
-
+    try:
+        user_summary = (
+            user_data['name'], user_data['url'], user_data['public_repos'],
+            user_data['followers'], user_data['following']
+        )
+    except KeyError:
+        raise
     return user_summary
 
 
-user_summary = collect_user_summary(user_data[0])
+# user_summary = collect_user_summary(user_data[0])
 
 
+# TODO: colocar exception customizada
 def collect_user_repositories(repositories):
-    list_repo = []
-    for repo in repositories:
-        list_repo.append(repo['name'])
-
-    return list_repo
-
-
-repository_list = collect_user_repositories(user_data[1])
+    try:
+        list_repo = [repo['name'] for repo in repositories]
+    except KeyError:
+        raise
+    return tuple(list_repo)
 
 
-def data_dump_to_txt(user_summary, repositories_list):
-    with open('test.txt', 'w') as f:
-        f.write(
-            f"{user_summary[0]}, {user_summary[1]}, {user_summary[2]},"
-            f" {user_summary[3]}, {user_summary[4]}, {user_summary[5]},"
-            f" {repositories_list}"
-        )
+# repository_list = collect_user_repositories(user_data[1])
+
+
+def data_dump_to_txt(user_summary: tuple, repository_list: tuple) -> None:
+    try:
+        with open('github_user_dossier.txt', 'w') as f:
+            f.write(
+                f"{user_summary[0]}, {user_summary[1]}, {user_summary[2]},"
+                f" {user_summary[3]}, {user_summary[4]}, {repository_list}"
+            )
+    except IndexError:
+        raise
+    return None
+
+
+# data_dump_to_txt(user_summary, repository_list)
--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-import main
+from main import (
+    ask_user, collect_user_repositories, collect_user_summary, data_dump_to_txt, request_github,)
+
+import os
+from os.path import dirname, join
+from dotenv import load_dotenv
+dotenv_path = join(dirname(__file__), ".env")
+load_dotenv(dotenv_path)
+
+
 if __name__ == "__main__":
+    user_name = ask_user()
+    user_data = request_github(user_name)
+    user_summary = collect_user_summary(user_data[0])
+    repository_list = collect_user_repositories(user_data[1])
+    data_dump_to_txt(user_summary, repository_list)
+
     print("Terminou")
