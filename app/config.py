f87ecb1d3ebcd72995e4eba7e6107ff89c11738a
victorbrittoferreira@gmail.com <victorbrittoferreira@gmail.com>
2023-04-04 15:14:25 -0300

    basic config data updated

--- app/config.py
+++ app/config.py
@@ -8,4 +8,17 @@ load_dotenv(dotenv_path)
 
 
 class ConfigApp:
+    """
+    Import configs
+    """
+
     GITHUB_USER_NAME = os.environ.get("GITHUB_USER_NAME")
+
+    def __str__(self) -> str:
+        return f"""
+        Config App:
+            Client user_name: {self.GITHUB_USER_NAME}
+        """
+
+    def __repr__(self) -> str:
+        return f"ConfigApp(user_name={self.GITHUB_USER_NAME})"
